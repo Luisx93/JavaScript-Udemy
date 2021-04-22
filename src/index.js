5ed2aad5a1f201eb4dacfb0ff1b9c99880d1226e
José Luis <luisgeek93@gmail.com>
2021-04-22 11:37:00 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Desestructuración
+// Asignación Desestructurante
+const persona = {
+    nombre: 'Tony',
+    edad: 45,
+    clave: 'Ironman'
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const { nombre, edad, clave } = persona;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(nombre);
+console.log(edad);
+console.log(clave);
